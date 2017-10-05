48a60c636846b09eadf106e75c0faf4356a58569
Jesus Esquivel <JAER@Jesuss-MacBook-Air.local>
2017-10-05 17:57:06 -0500

    Started building out the news page

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,29 @@
 import React, {PropTypes} from 'react';
-import LoginPage from './home/LoginPage.js';
-import JumboTron from './home/JumboTron.js';
-import NavBar from './home/NavBar.js'
+// import LoginPage from './home/LoginPage.js';
+// import JumboTron from './home/JumboTron.js';
+// import NavBar from './home/NavBar.js'
+import NewsPage from './news/NewsPage.js';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 
+      // <div>
+      // <NavBar />
+      // </div>
+      // <div>
+      // <JumboTron />
+      // </div>
+      // <div>
+      // <LoginPage />
+      // </div>
 
 
 class App extends React.Component {
   render() {
     return (
     <div>
-      <div>
-      <NavBar />
-      </div>
-      <div>
-      <JumboTron />
-      </div>
-      <div>
-      <LoginPage />
-      </div>
+     <MuiThemeProvider>
+      <NewsPage />
+    </MuiThemeProvider>
     </div>
     );
   }
--- src/components/news/NewsPage.js
+++ src/components/news/NewsPage.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import DrawerUI from './DrawerUI.js';
+import NavBarThree from './NavBarThree.js';
+import BottomFooterTwo from './BottomFooterTwo.js';
 
 class NewsPage extends React.Component {
-  constructor)(props) {
+  constructor(props) {
   super(props);
-  this.state = {}
+  this.state = {
+
+  }
 }
 
 render() {
   return (
     <div>
+    <div>
+    <NavBarThree />
+    </div>
+    <div>
+    <DrawerUI />
+    </div>
+    <div>
+      <BottomFooterTwo />
+    </div>
     </div>
     );
   }
 }
+
+export default NewsPage;
