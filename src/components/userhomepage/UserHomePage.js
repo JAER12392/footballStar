67c0de286d18eb5a202c6a1347ae10ecb171c737
Jesus Esquivel <JAER@cpe-172-100-1-29.twcny.res.rr.com>
2017-12-29 20:38:51 -0600

    Have adjusted the card titles under the videos and now working on the two maincards

--- src/components/userhomepage/UserHomePage.js
+++ src/components/userhomepage/UserHomePage.js
@@ -17,20 +17,36 @@ class UserHomePage extends React.Component {
           <div id="second-section">
               <div id="second-section-videos">
               <VideoOne />
-              <Col className="notecardone" l={4} m={6} s={12}>
-                  <Card className='red' offset="s1" textClassName='white-text' title='Card title'>
-                  I am a very simple card.
-                  </Card>
-              </Col>
-              <VideoTwo />
-
-              <Col className="notecardtwo" l={4} m={6} s={12}>
-                  <Card className='red' textClassName='white-text' title='Card title'>
-                  I am a very simple card.
+              <div className="notecardone">
+              <Col l={4} m={6} s={12}>
+                  <Card className='red' offset="s1" textClassName='white-text' title='Predicting The 2017-18 Premier League Season'>
+                    The Premier League is back! Who will win the title in 2017-18? Who will finish in the coveted Champions League spots? Which the three clubs will go down? And who will finish as top scorer and player of the year?
                   </Card>
               </Col>
               </div>
+              <VideoTwo />
+              <div className="notecardtwo">
+              <Col l={4} m={6} s={12}>
+                  <Card className='red' textClassName='white-text' title='Reaction to the 2018 World Cup Draw'>
+                  The draw for the group stage of the 2018 FIFA World Cup is complete.
+                  With the 2018 FIFA World Cup around the corner we will discuss who are the favorites and who can become the dark horse of the tournmanent.
+                  </Card>
+              </Col>
+              </div>
+              </div>
               <div id="second-section-notecards">
+              <Col l={4} m={6} s={12}>
+                  <Card className='red' textClassName='white-text' title='Reaction to the 2018 World Cup Draw'>
+                  The draw for the group stage of the 2018 FIFA World Cup is complete.
+                  With the 2018 FIFA World Cup around the corner we will discuss who are the favorites and who can become the dark horse of the tournmanent.
+                  </Card>
+              </Col>
+              <Col l={4} m={6} s={12}>
+                  <Card className='red' textClassName='white-text' title='Reaction to the 2018 World Cup Draw'>
+                  The draw for the group stage of the 2018 FIFA World Cup is complete.
+                  With the 2018 FIFA World Cup around the corner we will discuss who are the favorites and who can become the dark horse of the tournmanent.
+                  </Card>
+              </Col>
               </div>
 
           </div>
